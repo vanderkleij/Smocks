bab82287501257c84e9f3ec39f3019ae5a9d3f8d
Tom van der Kleij <aajvdkleij@gmail.com>
2015-06-07 13:41:52 +0200

    - Fixed nuspec - Added build script

---
+++ Build/Build.ps1
@@ -0,0 +1,24 @@
+param(
+	[Parameter(Mandatory=$true)][string]$solutionName,
+	[string]$configuration="Debug"
+)
+
+$baseDir  = resolve-path ..
+$sourceDir = "$baseDir\Source"
+$targets = "Clean", "Build"
+
+$msbuild = (Get-Command msbuild.exe -errorAction SilentlyContinue | Select -Property Definition).Definition
+
+if (-Not $msbuild) {
+	Write-Host "MSbuild not found!"
+	return
+}
+
+# Manual cleaning, since the "clean" target does not seem to delete contents of the 'obj' folder generated
+# by the solutions for other framework versions.
+Write-Host "Cleaning solution..."
+Get-ChildItem -include "obj","bin" -recurse $sourceDir | ?{ $_.PSIsContainer } | foreach ($_) {Remove-Item -Recurse -Force $_.fullname}
+
+$targetsOption = "/t:" + [string]::Join(",", $targets)
+Write-Host $targetsOption
+& $msbuild "$sourceDir\$solutionName" "/property:Configuration=$configuration" $targetsOption
